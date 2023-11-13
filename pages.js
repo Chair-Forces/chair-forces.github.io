f29200b9956019c219defca3ac4cc820e3b26897
@FFITDan <danieltuckerrust@gmail.com>
2023-11-13 20:07:08 +0000

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://github.com/chair-forces/chair-forces.github.io.git";
 
 ghpages.publish(
   pathname,
